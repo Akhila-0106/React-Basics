5ca02b99a322ad8fd65d9688cee95c98d1a363f2
Akhila-0106 <132571838+Akhila-0106@users.noreply.github.com>
2023-06-20 15:57:04 +0530

    adds on set simple calculator

--- React-Hooks/useState/usestate-twomultiple-peviouscount/src/App.js
+++ React-Hooks/useState/usestate-twomultiple-peviouscount/src/App.js
@@ -1,9 +1,35 @@
-const React , {useState} from "react"
+import React , {useState} from "react"
 
-const App = ()=>{
+
+const App=()=>{
     return(
         <div>
             <Calculator/>
+        </div>
+    )
+}
+
+const Calculator=()=>{
+    const [value,setValue]=useState(0)
+   
+    const onAdd = () => {
+        setValue(prevValue => prevValue + 1)
+      }
+    
+    const onSub = () => {
+        setValue(prevValue => prevValue - 1)
+    }
+
+    const onMul = () => {
+        setValue(prevValue => prevValue * 2)
+    }
+    return(
+        <div>
+            <h1>Simple Calculator</h1>
+            <h1>{value}</h1>
+            <button onClick={onAdd}>Add</button>
+            <button onClick={onSub}>Sub</button>
+            <button onClick={onMul}>Mul</button>
         </div>
     )
 }
